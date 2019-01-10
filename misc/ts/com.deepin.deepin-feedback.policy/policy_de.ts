489f1af168f30fd37fdd150d56fdc52a8ea37676
transifex <transifex@linuxdeepin.com>
2019-01-10 01:43:23 +0000

    auto sync po files from transifex
    Change-Id: I35fe26aab4768df07f3d1104481271543d800e2c

--- misc/ts/com.deepin.deepin-feedback.policy/policy_de.ts
+++ misc/ts/com.deepin.deepin-feedback.policy/policy_de.ts
@@ -4,12 +4,12 @@
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!message" line="0"/>
 			<source>To better solve system problems, log files will be collected for debugging, but user sensitive information will not be involved.</source>
-			<translation>Um Systemprobleme besser lösen zu können, werden Log-Dateien für das Debugging gesammelt. Sensitive Informationen werden dabei nicht berücksichtigt.</translation>
+			<translation>Um Systemprobleme besser lösen zu können, werden Protokolldateien zur Fehleranalyse  gesammelt. Sensitive Informationen werden dabei nicht berücksichtigt.</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!description" line="0"/>
 			<source>Authentication is required to collect system log</source>
-			<translation>Du musst dein Passwort eingeben, um das Aufzeichnen des System Logs zu erlauben.</translation>
+			<translation>Du musst dein Passwort eingeben, damit das Systemprotokoll gespeichert und an dein Feedback angehängt werden kann.</translation>
 		</message>
 	</context>
 </TS>
--- misc/ts/com.deepin.deepin-feedback.policy/policy_zh_TW.ts
+++ misc/ts/com.deepin.deepin-feedback.policy/policy_zh_TW.ts
@@ -4,12 +4,12 @@
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!message" line="0"/>
 			<source>To better solve system problems, log files will be collected for debugging, but user sensitive information will not be involved.</source>
-			<translation type="unfinished"/>
+			<translation>為了要更好的解決系統問題，日誌檔案將會蒐集偵錯所需資訊，但不會涉及到使用者的個人資訊。</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!description" line="0"/>
 			<source>Authentication is required to collect system log</source>
-			<translation>需要驗證</translation>
+			<translation>蒐集系統日誌檔案需要驗證</translation>
 		</message>
 	</context>
 </TS>
