4b47d9916cb2cb3679de60b954cdd2e7cadc9166
electricface1 <songwentai@deepin.com>
2019-07-05 08:15:45 +0000

    chore: auto pull translation files from transifex

--- misc/ts/com.deepin.deepin-feedback.policy/policy_fi.ts
+++ misc/ts/com.deepin.deepin-feedback.policy/policy_fi.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_419" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!message" line="0"/>
 			<source>To better solve system problems, log files will be collected for debugging, but user sensitive information will not be involved.</source>
-			<translation>Para resolver mejor los problemas del sistema, los archivos de registro se recopilarán para la depuración, pero la información confidencial del usuario no estará involucrada.</translation>
+			<translation>Jotta järjestelmäongelmat voitaisiin ratkaista paremmin, kerätään lokitiedostoja virheenkorjausta varten, mutta käyttäjän arkaluonteisia tietoja ei oteta mukaan.</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!description" line="0"/>
 			<source>Authentication is required to collect system log</source>
-			<translation>La autenticación es necesaria para recopilar el registro del sistema</translation>
+			<translation>Todennus tarvitaan järjestelmän lokin keräämiseksi</translation>
 		</message>
 	</context>
 </TS>
--- misc/ts/com.deepin.deepin-feedback.policy/policy_ko.ts
+++ misc/ts/com.deepin.deepin-feedback.policy/policy_ko.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_419" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ko" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!message" line="0"/>
 			<source>To better solve system problems, log files will be collected for debugging, but user sensitive information will not be involved.</source>
-			<translation>Para resolver mejor los problemas del sistema, los archivos de registro se recopilarán para la depuración, pero la información confidencial del usuario no estará involucrada.</translation>
+			<translation>시스템 문제를 더 잘 해결하기 위해 디버깅을 위해 로그 파일이 수집되지만 사용자 중요 정보는 포함되지 않습니다.</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.deepin-feedback-cli!description" line="0"/>
 			<source>Authentication is required to collect system log</source>
-			<translation>La autenticación es necesaria para recopilar el registro del sistema</translation>
+			<translation>시스템 로그를 수집하려면 인증이 필요합니다</translation>
 		</message>
 	</context>
 </TS>
